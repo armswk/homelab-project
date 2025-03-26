46b02242762b4ef4c5f68397115dabdbd2aee57f
armswk <siwakorn.s@yahoo.com>
2025-03-26 13:53:20 +0100

    terraform s3 static website deployment

---
+++ Learning_Phase/terraform/s3-static-website/s3.tf
@@ -0,0 +1,62 @@
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+#Creating S3 Bucket
+resource "aws_s3_bucket" "static_website" {
+  bucket = "siwa-terraform-course-project-static-website-${random_id.bucket_suffix.hex}"
+}
+
+##Disabling Public Access Block
+resource "aws_s3_bucket_public_access_block" "static_website" {
+  bucket                  = aws_s3_bucket.static_website.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+##Configure S3 Bucket Policy
+resource "aws_s3_bucket_policy" "static_website_public_read" {
+  bucket = aws_s3_bucket.static_website.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.static_website.arn}/*"
+      }
+    ]
+    }
+  )
+}
+
+##Configure S3 Static Website
+resource "aws_s3_bucket_website_configuration" "static_website" {
+  bucket = aws_s3_bucket.static_website.id
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "error.html"
+  }
+}
+
+##Upload the webpage's resources
+resource "aws_s3_object" "index_html" {
+  bucket       = aws_s3_bucket.static_website.id
+  key          = "index.html"
+  source       = "build/index.html"
+  etag         = filemd5("build/index.html")
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "error_html" {
+  bucket       = aws_s3_bucket.static_website.id
+  key          = "error.html"
+  source       = "build/error.html"
+  etag         = filemd5("build/error.html")
+  content_type = "text/html"
+}
